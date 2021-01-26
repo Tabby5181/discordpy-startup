adfb704658bdb1e84f25c1f4a417c5193d94ec5a
Tabby5181 <78044414+Tabby5181@users.noreply.github.com>
2021-01-27 04:27:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,26 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+async def on_ready():
+print ("Ready")
+
+
+@bot.command(pass_context=True)
+async def join(ctx):
+author = ctx.message.author
+channel = author.voice_channel
+await bot.join_voice_channel(channel)
+
+    
+@client.event
+async def on_message(message):
+    if message.content == ".mute":
+        if message.author.guild_permissions.administrator:
+            bot_vc = message.guild.me.voice.channel # botのいるボイスチャンネルを取得
+            for member in bot_vc.members:
+                await member.edit(mute=True) # チャンネルの各参加者をミュートする
+        else:
+            await message.channel.send("実行できません。")
+
 
 bot.run(token)
