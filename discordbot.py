c17a4949890c5babe850a589ce93efbf15b644af
Tabby5181 <78044414+Tabby5181@users.noreply.github.com>
2021-01-27 04:56:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,9 +6,12 @@ import asyncio
 
 bot = commands.Bot(command_prefix="/")
 client = discord.Client()
+TOKEN = 'ODAzNzAwOTI5Mjk4OTU2Mjg4.YBBm0Q.VVNNVTnfZGNRrytlbpW0v3QNdVc'
 
+@client.event
 async def on_ready():
-print ("Ready")
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
 @bot.command(pass_context=True)
 async def join(ctx):
